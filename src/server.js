29b471d5eaa931b2c53f0eb341b438f665e1743b
agumartinez1209 <agu.am.2359@gmail.com>
2022-09-01 00:12:52 -0300

    modifico prodcuto y carrito

--- src/server.js
+++ src/server.js
@@ -44,20 +44,51 @@ const productosRouter = new Router()
 productosRouter.use(express.json());
 productosRouter.use(express.urlencoded({extended:true}));
 
-productosRouter.get('/', async (req, res) => { 
-    res.json(await productosApi.getAll())
-})
-productosRouter.get('/:id', async (req, res) => { 
-            res.json( await productosApi.listar(req.params.id))
-})
-productosRouter.post('/', soloAdmins, async (req, res) => { 
-        res.json(await productosApi.guardar(req.body))
-})
-productosRouter.put('/:id', soloAdmins, async (req, res) => { 
-        res.json(await productosApi.actualizar(req.body , req.params.id))
-})
-productosRouter.delete('/:id', soloAdmins, async (req, res) => { 
-        res.json(await productosApi.borrar(req.params.id))    
+productosRouter.get('/productos', soloAdmins,async  (req, res) => {
+    try {
+        const prod = await productosApi.getAll();
+        res.json(prod);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+productosRouter.get('/productos/:id',soloAdmins ,existe, async (req, res) => {
+    const id = req.params.id;
+    const prod = await productosApi.getById(id)
+    console.log('aca probamos el id ' + id);
+    res.json(prod);
+});
+
+productosRouter.post('/productos',soloAdmins, (req, res) => {
+    try {
+        prod = req.body;
+        prod.timestamp = Date.now();
+        productosApi.save(prod);
+        res.json(req.body);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+productosRouter.put('/productos/:id',soloAdmins ,existe,(req, res)=> {
+    try {
+        console.log(req.body, req.params.id);
+        productosApi.update(req.body, req.params.id);
+        res.send(productosApi.getAll());
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+productosRouter.delete('/productos/:id', existe, (req, res) => {
+    try {
+        const id = req.params.id;
+        productosApi.deleteById(id);
+        res.json(productosApi.getAll());
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 //--------------------------------------------
@@ -68,25 +99,92 @@ const carritosRouter = new Router()
 carritosRouter.use(express.json())
 carritosRouter.use(express.urlencoded({extended:true}))
 
-carritosRouter.post('/', async(req, res)=>{
-    res.json(await carritosApi.guardar(req.body))
+carritosRouter.post('/carrito', async (req, res) => {
+    try {
+       const carrito = {};
+       carrito.timestamp = Date.now();
+       carrito.productos = []; 
+       res.json(await carritosApi.save(carrito));
+    } catch (error) {
+      console.log(error);  
+    }
 })
-carritosRouter.delete('/:id', async (req, res) => { 
-    res.json(await carritosApi.borrar(req.params.id))    
+
+carritosRouter.delete('/carrito/:id', (req, res) => {
+    try {
+        const id = req.params.id;
+        carritosApi.deleteById(id);
+        res.json(carritosApi.getAll());
+    } catch (error) {
+        console.log(error);
+    }
 })
-carritosRouter.get('/:id/productos', async (req, res) => { 
-    res.json(await carritosApi.listar(req.params.id))
+
+carritosRouter.get('/carrito/:id/productos', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const carrito = await carritosApi.getById(id);
+        res.json(carrito); 
+    } catch (error) {
+        console.log(error);
+    }
+   
 })
-carritosRouter.post('/:id/productos/:id_prod', async (req, res) => { 
-    const carrito = await carritosApi.listar(req.params.id);
-    carrito['productos'].push( await productosApi.listar(req.params.id_prod))
-    res.json(await carritosApi.actualizar(carrito,req.params.id))
+
+carritosRouter.get('/carrito', async (req, res) => {
+    try {
+        const carritos = await carritosApi.getAll();
+        res.json(carritos);
+    } catch (error) {
+       console.log(error); 
+    }
 })
-carritosRouter.delete('/:id/productos/:id_prod', async (req, res) => {
-    const carrito = await carritosApi.listar(req.params.id);
-    const indexId = carrito['productos'].findIndex(p => p.id == req.params.id_prod )
-    carrito['productos'].splice( indexId  , 1);
-   res.json(await carritosApi.actualizar(carrito,req.params.id))
+
+carritosRouter.get('/carrito/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const carritos = await carritosApi.getById(id);
+        res.json(carritos);
+    } catch (error) {
+       console.log(error); 
+    }
+})
+
+carritosRouter.post('/carrito/:id/productos', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const prod = req.body.id;
+    const miprod = await productosApi.getById(prod);
+    const carritos = await carritosApi.getById(id);
+    carritos.productos.push(miprod);
+    carritosApi.update(carritos, id);
+    res.json(carritos);
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+carritosRouter.delete('/carrito/:id/productos/:id_prod', (req, res) => {
+    try {
+        const id = req.params.id;
+        const idProd = req.params.id_prod;
+        const carrito = carritosApi.getById(id);
+        const prod = carrito.productos.filter(p => p.id != parseInt(idProd));
+        carrito.productos = prod;
+        carritosApi.update(carrito, id);
+    } catch (error) {
+        console.log(error);
+    }
+    
+})
+
+
+carritosRouter.delete('/carrito', async (req, res) => {
+    try {
+        carritosApi.deleteAll();
+    } catch (error) {
+        console.log(error)
+    }
 })
 
 //--------------------------------------------
